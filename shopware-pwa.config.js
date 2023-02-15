63256697913101cdffe2e530c53e2102893bf560
Bruna Scauri <brunascauri@hotmail.com>
2023-02-15 14:51:00 -0300

    chore: pwa.config added to gitignore

--- shopware-pwa.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  shopwareEndpoint: "https://pwa-demo-api.shopware.com/trunk",
-  shopwareAccessToken: "SWSC40-LJTNO6COUEN7CJMXKLA",
-  shopwareDomainsAllowList: ["https://pwa-demo-api.shopware.com/trunk/", ]
-};
